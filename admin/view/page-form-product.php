33087359dc383a72d30172af99582b695ce8345e
Huu Nghia <nguyenhuunghia089@gmail.com>
2023-11-18 16:34:43 +0700

    update

--- admin/view/page-form-product.php
+++ admin/view/page-form-product.php
@@ -1,100 +1,97 @@
 <section class="content-main">
-            <div class="row">
-                <div class="col-12">
-                    <div class="content-header">
-                        <h2 class="content-title">Thêm sản phẩm mới</h2>
-                        <div>
-                            <button class="btn btn-md rounded font-sm hover-up">Lưu</button>
-                        </div>
+    <div class="row">
+        <form action="#" method="post" enctype="multipart/form-data">
+            <div class="col-12">
+                <div class="content-header">
+                    <h2 class="content-title">Thêm sản phẩm mới</h2>
+                    <div>
+                        <button class="btn btn-md rounded font-sm hover-up">Lưu</button>
                     </div>
-                </div>
-                <div class="col-lg-7">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <h4>Thông tin sản phẩm</h4>
-                        </div>
-                        <div class="card-body">
-                            <form>
-                                <div class="mb-4">
-                                    <label class="form-label">Tên Sản Phẩm</label>
-                                    <input type="text" placeholder="Type here" class="form-control" id="product_name">
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-4">
-                                        <div class="mb-4">
-                                            <label class="form-label">Giá Gốc</label>
-                                            <div class="row gx-2">
-                                                <input placeholder="VND" type="text" class="form-control">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="mb-4">
-                                            <label class="form-label">Giá Đã Giảm</label>
-                                            <input placeholder="VND" type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="mb-4">
-                                    <label class="form-label" for="">Mô tả 1</label>
-                                    <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
-                                </div>
-                                <div class="mb-4">
-                                    <label class="form-label" for="">Mô tả 2</label>
-                                    <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
-                                </div>
-                                <div class="mb-4">
-                                    <label class="form-label">Besteller</label>
-                                    <input type="checkbox" class="form-control1" id="product_bestseller">
-
-                                    <label class="form-label">New</label>
-                                    <input type="checkbox" class="form-control1" id="product_new">
-
-                                    <label class="form-label">Hot</label>
-                                    <input type="checkbox" class="form-control1" id="product_hot">
-                                </div>
-                            
-                            </form>
-                        </div>
-                    </div>
-                    <!-- card end// -->
-                    <!-- card end// -->
-                </div>
-                <div class="col-lg-5">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <h4>Hình ảnh</h4>
-                        </div>
-                        <div class="card-body">
-                            <div class="input-upload">
-                                <img src="assets/imgs/theme/upload.svg" alt="">
-                                <input class="form-control" type="file">
-                            </div>
-                        </div>
-                    </div>
-                    <!-- card end// -->
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <h4>Danh Mục</h4>
-                        </div>
-                        <div class="card-body">
-                            <div class="row gx-2">
-                                <div class="col-sm-6 mb-3">
-                                    <label class="form-label">Danh Mục</label>
-                                    <select class="form-select">
-                                        <option> Automobiles </option>
-                                        <option> Home items </option>
-                                        <option> Electronics </option>
-                                        <option> Smartphones </option>
-                                        <option> Sport items </option>
-                                        <option> Baby and Tous </option>
-                                    </select>
-                                </div>
-                            </div>
-                            <!-- row.// -->
-                        </div>
-                    </div>
-                    <!-- card end// -->
                 </div>
             </div>
-        </section>
+            <div class="col-lg-7">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h4>Thông tin sản phẩm</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="mb-4">
+                            <label class="form-label">Tên Sản Phẩm</label>
+                            <input type="text" placeholder="Type here" class="form-control" id="product_name">
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="mb-4">
+                                    <label class="form-label">Giá Gốc</label>
+                                    <div class="row gx-2">
+                                        <input placeholder="VND" type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="mb-4">
+                                    <label class="form-label">Giá Đã Giảm</label>
+                                    <input placeholder="VND" type="text" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="">Mô tả 1</label>
+                            <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="">Mô tả 2</label>
+                            <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label">Besteller</label>
+                            <input type="checkbox" class="form-control1" id="product_bestseller">
+                            <label class="form-label">New</label>
+                            <input type="checkbox" class="form-control1" id="product_new">
+                            <label class="form-label">Hot</label>
+                            <input type="checkbox" class="form-control1" id="product_hot">
+                        </div>
+                    </div>
+                </div>
+                <!-- card end// -->
+                <!-- card end// -->
+            </div>
+            <div class="col-lg-5">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h4>Hình ảnh</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="input-upload">
+                            <img src="assets/imgs/theme/upload.svg" alt="">
+                            <input class="form-control" type="file">
+                        </div>
+                    </div>
+                </div>
+                <!-- card end// -->
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h4>Danh Mục</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="row gx-2">
+                            <div class="col-sm-6 mb-3">
+                                <label class="form-label">Danh Mục</label>
+                                <select class="form-select">
+                                    <option> Automobiles </option>
+                                    <option> Home items </option>
+                                    <option> Electronics </option>
+                                    <option> Smartphones </option>
+                                    <option> Sport items </option>
+                                    <option> Baby and Tous </option>
+                                </select>
+                            </div>
+                        </div>
+                        <!-- row.// -->
+                    </div>
+                </div>
+                <!-- card end// -->
+            </div>
+        </form>
+    </div>
+</section>
