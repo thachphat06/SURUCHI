d452350446453e02b41a2224a094a76ee8757b98
Huu Nghia <nguyenhuunghia089@gmail.com>
2023-11-21 13:06:00 +0700

    update database

--- suruchi.sql
+++ suruchi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2023 at 09:43 AM
+-- Generation Time: Nov 21, 2023 at 07:05 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
